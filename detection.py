40b5b8d07ccbc74647425c41770dddfde5062c4e
Trần Hữu <huutrank4ds@gmail.com>
2025-08-13 12:09:39 +0700

    Replace test_net: batched img BGR

--- detection.py
+++ detection.py
@@ -6,49 +6,52 @@ import cv2
 import craft_utils
 import numpy as np
 
-def test_net(net, image, text_threshold=0.7, link_threshold=0.5, low_text=0.4, cuda=False, poly=False,
-             canvas_size=1280, mag_ratio=1.5, refine_net=None):
-    """
-    Hàm test_net đã được chỉnh sửa để loại bỏ việc tính toán thời gian
-    và sử dụng tham số thay vì biến toàn cục `args`.
-    """
-    # resize
-    img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, canvas_size, interpolation=cv2.INTER_LINEAR, mag_ratio=mag_ratio)
-    ratio_h = ratio_w = 1 / target_ratio
+def test_net(canvas_size, mag_ratio, net, image, text_threshold, link_threshold, low_text, poly, device, estimate_num_chars=False):
+    if isinstance(image, np.ndarray) and len(image.shape) == 4:  # image is batch of np arrays
+        image_arrs = image
+    else:                                                        # image is single numpy array
+        image_arrs = [image]
 
+    img_resized_list = []
+    # resize
+    for img in image_arrs:
+        img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(img, canvas_size,
+                                                                    interpolation=cv2.INTER_LINEAR,
+                                                                    mag_ratio=mag_ratio)
+        img_resized_list.append(img_resized)
+    ratio_h = ratio_w = 1 / target_ratio
     # preprocessing
-    x = imgproc.normalizeMeanVariance(img_resized)
-    x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
-    x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
-    if cuda:
-        x = x.cuda()
+    x = [np.transpose(imgproc.normalizeMeanVariance(n_img), (2, 0, 1))
+        for n_img in img_resized_list]
+    x = torch.from_numpy(np.array(x))
+    x = x.to(device)
 
     # forward pass
     with torch.no_grad():
         y, feature = net(x)
 
-    # make score and link map
-    score_text = y[0,:,:,0].cpu().data.numpy()
-    score_link = y[0,:,:,1].cpu().data.numpy()
+    boxes_list, polys_list = [], []
+    for out in y:
+        # make score and link map
+        score_text = out[:, :, 0].cpu().data.numpy()
+        score_link = out[:, :, 1].cpu().data.numpy()
 
-    # refine link
-    if refine_net is not None:
-        with torch.no_grad():
-            y_refiner = refine_net(y, feature)
-        score_link = y_refiner[0,:,:,0].cpu().data.numpy()
+        # Post-processing
+        boxes, polys, mapper = craft_utils.getDetBoxes(
+            score_text, score_link, text_threshold, link_threshold, low_text, poly, estimate_num_chars)
 
-    # Post-processing
-    boxes, polys = craft_utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+        # coordinate adjustment
+        boxes = craft_utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
+        polys = craft_utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
+        if estimate_num_chars:
+            boxes = list(boxes)
+            polys = list(polys)
+        for k in range(len(polys)):
+            if estimate_num_chars:
+                boxes[k] = (boxes[k], mapper[k])
+            if polys[k] is None:
+                polys[k] = boxes[k]
+        boxes_list.append(boxes)
+        polys_list.append(polys)
 
-    # coordinate adjustment
-    boxes = craft_utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
-    polys = craft_utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
-    for k in range(len(polys)):
-        if polys[k] is None: polys[k] = boxes[k]
-
-    # render results (optional)
-    render_img = score_text.copy()
-    render_img = np.hstack((render_img, score_link))
-    ret_score_text = imgproc.cvt2HeatmapImg(render_img)
-
-    return boxes, polys, ret_score_text
+    return boxes_list, polys_list
