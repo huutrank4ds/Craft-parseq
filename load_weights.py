4fddbefd60f5596efe6c7bbaa23979e76b5fab2f
Trần Hữu <huutrank4ds@gmail.com>
2025-08-13 11:07:54 +0700

    Add test_net from detection.py

--- load_weights.py
+++ load_weights.py
@@ -1,4 +1,7 @@
 from collections import OrderedDict
+import torch
+from craft import CRAFT
+import torch.backends.cudnn as cudnn
 
 def copyStateDict(state_dict):
     if list(state_dict.keys())[0].startswith("module"):
@@ -10,3 +13,21 @@ def copyStateDict(state_dict):
         name = ".".join(k.split(".")[start_idx:])
         new_state_dict[name] = v
     return new_state_dict
+
+def get_pretrained_model(trained_model, device='cpu', quantize=True, cudnn_benchmark=False):
+    net = CRAFT()
+
+    if device == 'cpu':
+        net.load_state_dict(copyStateDict(torch.load(trained_model, map_location=device, weights_only=False)))
+        if quantize:
+            try:
+                torch.quantization.quantize_dynamic(net, dtype=torch.qint8, inplace=True)
+            except:
+                pass
+    else:
+        net.load_state_dict(copyStateDict(torch.load(trained_model, map_location=device, weights_only=False)))
+        net = torch.nn.DataParallel(net).to(device)
+        cudnn.benchmark = cudnn_benchmark
+
+    net.eval()
+    return net
